6cdd6d92b61e65f94403b993722f0d3cabfc405f
angelikimodifi <angeliki.patrinou@modifi.com>
2019-02-02 23:21:25 +0100

    rename variables

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 
-$('.decimalToRoman').on('input',()=>{
-  if(!/^\d*$/.test($('input').val())){
-    $('#output')[0].innerText = ''
-    $('#error')[0].innerText = 'Please enter a number'
+$('.decimalToRoman').on('input', () => {
+
+  let inputValue = $('.decimalToRoman').val()
+  console.log(inputValue);
+  //if input value is not a number
+  if(!/^\d*$/.test(inputValue)){
+    $('#outputDecimal')[0].innerText = ''
+    $('#errorDecimal')[0].innerText = 'Please enter a number'
   }else{
-    if($('input').val().length > 4){
-      $('#output')[0].innerText = ''
-      $('#error')[0].innerText = 'Number too long'
+    if(inputValue > 12000){
+      $('#errorDecimal')[0].innerText = 'Number too long'
+      $('#outputDecimal')[0].innerText = ''
     }else{
-      $('#error')[0].innerText = ''
-      $('#output')[0].innerText = decimalToRoman($('.decimalToRoman').val())
+      $('#errorDecimal')[0].innerText = ''
+      $('#outputDecimal')[0].innerText = decimalToRoman(inputValue)
     }
   }
 })
 
-$('.romanToDecimal').on('input',()=>{
-  let lastInputedChar = $('.romanToDecimal').val().slice($('.romanToDecimal').val().length-1,$('.romanToDecimal').val().length)
+$('.romanToDecimal').on('input', () => {
+
+  let inputValue = $('.romanToDecimal').val()
+  let lastInputedChar = inputValue.slice(inputValue.length-1, inputValue.length)
+
   if(['I','V','X','L','C','D','M'].includes(lastInputedChar.toUpperCase())){
     $('#errorRoman')[0].innerText = ''
-    $('#outputRoman')[0].innerText = romanToDecimal($('.romanToDecimal').val().toUpperCase())
+    $('#outputRoman')[0].innerText = romanToDecimal(inputValue.toUpperCase())
   }else{
     if($('.romanToDecimal').val()==''){
       $('#outputRoman')[0].innerText = ''
